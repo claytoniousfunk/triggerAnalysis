8452e10ea78fb0533e99b03a474297d20c7c062d
clayton <clayton@debian>
2025-11-02 10:14:29 -0600

    look at different trigger

--- triggerAnalysisPrompt.C
+++ triggerAnalysisPrompt.C
@@ -473,7 +473,7 @@ void triggerAnalysisPrompt(int file_i = 1){
     triggerDecision_100 = triggerDecisions.at(3);
     triggerDecision_120 = triggerDecisions.at(4);
 
-    if(triggerDecision_40==1) cout << "triggerDecision_40 = " << triggerDecisions.at(0) << endl;
+    if(triggerDecision_100==1) cout << "triggerDecision_100 = " << triggerDecisions.at(3) << endl;
 
 
     if(triggerDecision_40==1) {// only fill the numerator if the trigger is on.
