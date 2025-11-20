64e113ea0db964b1019fe20c8be843ef805fc190
clayton <clayton@debian>
2025-11-20 14:31:59 -0600

    more couts

--- triggerAnalysisPrompt_PbPb.C
+++ triggerAnalysisPrompt_PbPb.C
@@ -273,9 +273,11 @@ void triggerAnalysisPrompt_PbPb(int file_i = 1){
   for (Long64_t j_entry = 0; j_entry < entriesTmp; ++j_entry){
 
     std::cout << "processing event " << j_entry << "\n";
-
+    std::cout << "grabbing treeggHiNtuplizer " << j_entry << "\n";
     treeggHiNtuplizer->GetEntry(j_entry);
+    std::cout << "grabbing treeHiEvt " << j_entry << "\n";
     treeHiEvt->GetEntry(j_entry);
+    std::cout << "grabbing treeJet " << j_entry << "\n";
     treeJet->GetEntry(j_entry);
 
     //cout << "weight = " << weight << endl;
